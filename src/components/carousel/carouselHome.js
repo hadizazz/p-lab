6207d107b79799a13ab556f9eb5e0f272adfa767
hadizazz <hadizazz81@gmail.com>
2022-07-14 16:31:57 +0700

    final

---
+++ src/components/carousel/carouselHome.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function CarouselHome() {
+  return (
+    <div className="mb-5">
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <img
+            style={{ objectFit: "cover", height: "50rem" }}
+            className="img-fluid"
+            src="https://images.unsplash.com/photo-1472457847783-3d10540b03d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80"
+            alt="First slide"
+            width="100%"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            style={{ objectFit: "cover", height: "50rem" }}
+            className="img-fluid "
+            src="https://images.unsplash.com/photo-1625895197185-efcec01cffe0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt="Second slide"
+            width="100%"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            style={{ objectFit: "cover", height: "50rem" }}
+            className="img-fluid"
+            src="https://images.unsplash.com/photo-1621478374422-35206faeddfb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt="Third slide"
+            width="100%"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+export default CarouselHome;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import "../../assets/css/style.css";
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useCart } from "react-use-cart";
+
+const Navbars = () => {
+  const { totalItems } = useCart();
+  const [statusNavbar, setStatusNavbar] = useState(false);
+  const accessToken = localStorage.getItem("accessToken");
+  const tokens = () => {
+    if (accessToken) {
+      setStatusNavbar(true);
+    } else {
+      setStatusNavbar(false);
+    }
+  };
+  useEffect(() => {
+    const a = localStorage.getItem("accessToken");
+    tokens();
+  });
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setStatusNavbar(false);
+  };
+  return (
+    <div className="nvbr">
+      <Navbar bg="light" expand="md">
+        <Container fluid>
+          <Navbar.Brand className="offset-1 mr-5 pr-5" href="/">
+            ActionFigure
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse className="justify-content-end" id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link style={{ color: "#000" }} href="/">
+                Home
+              </Nav.Link>
+              <Nav.Link style={{ color: "#000" }} href="/shop">
+                Shop
+              </Nav.Link>
+            </Nav>
+            {statusNavbar ? (
+              <>
+                <div className="drpdwn">
+                  <NavDropdown title="Account" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item to="/login" onClick={logout}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </div>
+                <a className="btn btn-lg _cart" href="/cart" role="button">
+                  <AiOutlineShoppingCart />
+                  <span>{totalItems}</span>
+                </a>
+              </>
+            ) : (
+              <Nav.Link
+                className="mx-5"
+                style={{ color: "#000" }}
+                href="/login"
+              >
+                Login
+              </Nav.Link>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+export default Navbars;
